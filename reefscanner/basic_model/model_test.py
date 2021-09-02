b3a7d1ede046ba83b337aa2d0f07830c9f520200
gcoleman72 <g.coleman@aims.gov.au>
2021-09-02 16:46:22 +1000

    changes to progress tracking bulk operations

--- reefscanner/basic_model/model_test.py
+++ reefscanner/basic_model/model_test.py
@@ -1,22 +1,25 @@
 from reefscanner.basic_model.basic_model import BasicModel
+from reefscanner.basic_model.progress_no_queue import ProgressNoQueue
 from reefscanner.basic_model.progress_queue import ProgressQueue
 
 basic_model = BasicModel()
 basic_model.data_folder = "C:/aims/reef-scanner"
 
-progress_queue = ProgressQueue()
+# progress_queue = ProgressQueue()
+# basic_model.read_from_files(progress_queue)
+#
+# print(basic_model.sites_data_array)
+# print(basic_model.surveys_data_array)
+#
+# ok = True
+# while ok:
+#     try:
+#         i = progress_queue.q.get(block=False)
+#         print(str(i))
+#     except:
+#         ok = False
+
+progress_queue = ProgressNoQueue()
 basic_model.read_from_files(progress_queue)
-
-print(basic_model.sites_data_array)
-print(basic_model.surveys_data_array)
-
-ok = True
-while ok:
-    try:
-        i = progress_queue.q.get()
-        print(str(i))
-    except:
-        ok = False
-
 
 
