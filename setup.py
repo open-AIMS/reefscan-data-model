445a2b4f4b3e6378ce3a172a3d01adfdea067523
gcoleman72 <g.coleman@aims.gov.au>
2023-02-10 11:41:59 +1000

    update readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='reefscanner',
     packages=['reefscanner.basic_model', 'reefscanner.basic_model.samba', 'reefscanner.archive_stats'],
-    version='0.4.0i',
+    version='0.4.1',
     description='aims_reef_scanner_data_model',
     author='Greg',
     license='MIT',
