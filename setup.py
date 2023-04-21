806f0e10feb846650f111807397967832be9e4df
gcoleman72 <g.coleman@aims.gov.au>
2023-04-21 14:22:01 +1000

    Bug reading files from the camera

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='reefscanner',
     packages=['reefscanner.basic_model', 'reefscanner.basic_model.samba', 'reefscanner.archive_stats'],
-    version='1.0.3 ',
+    version='1.0.3.1 ',
     description='aims_reef_scanner_data_model',
     author='Greg',
     license='MIT',
